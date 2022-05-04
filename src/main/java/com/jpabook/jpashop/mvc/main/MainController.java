ed2688344513e2df7d8660618c5ffacfa0764d06
Aivyss <hklee6417@gmail.com>
2022-05-05 01:55:01 +0900

    update: class_diagram.plantuml

--- src/main/java/com/jpabook/jpashop/mvc/main/MainController.java
+++ src/main/java/com/jpabook/jpashop/mvc/main/MainController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	@GetMapping("/")
 	public String mainView(Model model) {
-		model.addAttribute("data", "hello");
-		log.info("give main view template");
 		return "main";
 	}
 }
